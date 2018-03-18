b14967d420022557eb6b506d5052e385de6ef749
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-03-17 23:42:21 -0400

    Added login authentication

--- Bookstore/src/model/AccountDAO.java
+++ Bookstore/src/model/AccountDAO.java
@@ -1,5 +1,43 @@
 package model;
+import java.sql.*;
 
 public class AccountDAO {
+	
+	private Connection conn;
 
+	/*
+	 * attempts to login using the given username and password
+	 * @returns true if successful login
+	 */
+	public boolean attemptLogin(String username, String password) throws SQLException {
+		
+		String query = "select * from account where username=?";
+		boolean result = false;
+		
+		try {
+			conn = DatabaseConnector.getDatabaseConnection();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		PreparedStatement p = conn.prepareStatement(query);
+		p.setString(1, username);
+		ResultSet r = p.executeQuery();
+		
+		//if the username is found check to see if the pw matches
+		if (r.next()) {
+			result = (r.getString("PASSWORD").equals(password));
+		}
+		else {
+			result = false;
+		}
+		
+		conn.close();
+		p.close();
+		r.close();
+		
+		return result;
+	}
+	
 }
