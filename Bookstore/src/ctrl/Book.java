d818910d5de24092f3611e921b5f0d753b155405
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-03-25 13:56:59 -0400

    Starting book page

---
+++ Bookstore/src/ctrl/Book.java
@@ -0,0 +1,59 @@
+package ctrl;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.BookBean;
+import model.BookDAO;
+
+/**
+ * Servlet implementation class Book
+ */
+@WebServlet("/Book")
+public class Book extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	BookDAO b;
+	
+	public void init() {
+		b = new BookDAO();
+	}
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Book() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int bid = Integer.parseInt(request.getParameter("bid"));
+		
+		try {
+			BookBean book = b.retrieveBookByBid(bid);
+			request.setAttribute("book", book);
+			request.getRequestDispatcher("book.jspx").forward(request, response);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
