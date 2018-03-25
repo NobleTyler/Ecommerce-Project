1a71b423f44ce9cb0341c3f2ee9465bd2fb4ae79
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-03-25 15:19:50 -0400

    Added rng for picking 6 random books for the front page

--- Bookstore/src/ctrl/Start.java
+++ Bookstore/src/ctrl/Start.java
@@ -2,6 +2,8 @@ package ctrl;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.BookBean;
 import model.*;
 
 /**
@@ -20,6 +23,7 @@ public class Start extends HttpServlet {
 
 	AccountDAO a;
 	BookDAO b;
+	List<Integer> allBooks;
 	
     /**
      * Default constructor. 
@@ -32,6 +36,33 @@ public class Start extends HttpServlet {
     	//initializes the model
     	a = new AccountDAO();
     	b = new BookDAO();
+    	try {
+    		allBooks = b.retrieveAllBooks();
+    	} catch (SQLException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    /*
+     * @return a random bid from the allBooks list
+     */
+    public int randomBook() {
+    	Random rand = new Random();
+    	int index = rand.nextInt(allBooks.size());
+    	return allBooks.get(index);
+    }
+    
+    public void setFrontPageBooks(HttpServletRequest request) {
+    	for (int i = 0; i < 6; i++) {
+			int bid = randomBook();
+			/*try {
+				BookBean book = b.retrieveBookByBid(bid);
+				request.setAttribute("book" + i, book);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}*/
+			request.setAttribute("randbook" + i, bid);
+		}
     }
 
 	/**
@@ -40,6 +71,8 @@ public class Start extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.getSession().removeAttribute("username");						//new session or user is logging out
+		
+		setFrontPageBooks(request);
 		request.getRequestDispatcher("Index.jspx").forward(request, response);
 	}
 
