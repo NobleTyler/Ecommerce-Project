cc3047951b33d04e060d0fcfafcf016689f468c6
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-04-12 14:52:09 -0400

    Finalized the review page so that people who have bought the book can add the review. Also modified the review.js file

--- Bookstore/WebContent/res/js/review.js
+++ Bookstore/WebContent/res/js/review.js
@@ -1,39 +1,44 @@
-     
-        function count_down(obj) {
-             
-            var element = document.getElementById('count2');
-             
-            element.innerHTML = 1000 - obj.value.length;
-             
-            if (100 - obj.value.length < 0) {
-                alert("Too much text")
-             
-            } else {
-                element.style.color = 'grey';
-                element.innerHTML =object.value.length + "/" +"1000"; 
-            }
-             
-        }
-        
-        function buttonIsPressed(){
-        	var ok=false;
-        	
-        	if(document.getElementById('5-star').checked) {
-        		  ok = true;
-        	}else if(document.getElementById('4-star').checked) {
-        		  ok = true;
-        	}else if(document.getElementById('3-star').checked) {
-      		  ok = true;
-        	}else if(document.getElementById('2-star').checked) {
-      		  ok = true;
-        	}else if(document.getElementById('1-star').checked) {
-      		  ok = true;
-        	}
-        	
-        	if(!ok){
-        		alert("Please give this book a rating")
-        		ok=false;
-        	}
-        	
-        	return ok;
-        }
+function count_down(obj) {
+
+	var element = document.getElementById('count2');
+
+	element.innerHTML = 1000 - obj.value.length;
+
+	if (100 - obj.value.length < 0) {
+		alert("Too much text")
+
+	} else {
+		element.style.color = 'grey';
+		element.innerHTML.value = object.value.length + "/" + "1000";
+	}
+
+}
+
+function buttonIsPressed() {
+	var ok = false;
+	
+	
+	if(document.getElementById('comment').value==="Enter some text here"){
+		document.getElementById('comment').value="";
+		//alert(document.getElementById('comment').value)
+	}
+	
+	if (document.getElementById('5-star').checked) {
+		ok = true;
+	} else if (document.getElementById('4-star').checked) {
+		ok = true;
+	} else if (document.getElementById('3-star').checked) {
+		ok = true;
+	} else if (document.getElementById('2-star').checked) {
+		ok = true;
+	} else if (document.getElementById('1-star').checked) {
+		ok = true;
+	}
+
+	if (!ok) {
+		alert("Please give this book a rating")
+		ok = false;
+	}
+	
+	return ok;
+}
