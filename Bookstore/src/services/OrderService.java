db4dfb5bbf9216ddcd326d2dfa899e5ff2c2b6a8
Matthew MacEachern <mcm.maceachern@gmail.com>
2018-04-14 00:05:48 -0400

    Added jersey jar and web xml mapping for rest service

---
+++ Bookstore/src/services/OrderService.java
@@ -0,0 +1,15 @@
+package services;
+
+//import javax.websocket.server.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+public class OrderService {
+	
+}
