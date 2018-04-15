64472a9681ed778850d5eec0e11367ce90a5c94f
SaeedS28 <awesomesaad.saeed2@gmail.com>
2018-04-15 18:14:26 -0400

    fixed the validation on the shopping cart inputs.

--- Bookstore/WebContent/res/js/cart.js
+++ Bookstore/WebContent/res/js/cart.js
@@ -1,15 +1,47 @@
 function validateInfo(){
-	var cfname = document.getElementById("cfname").value;
-	var clname = document.getElementById("clname").value;
-	var ccnum = document.getElementById("ccnum").value;
-	var exp = document.getElementById("expmonth").value;
-	var expyear = document.getElementById("expyear").value;
-	var ccv = document.getElementById("ccv").value;
+	var cFname = document.getElementById("cfname").value;
+	var cLname = document.getElementById("clname").value;
+	var ccNum = document.getElementById("ccnum").value;
+	var expMonth = document.getElementById("expmonth").value;
+	var expYear = document.getElementById("expyear").value;
+	var cvv = document.getElementById("cvv").value;
 	
 	errors=[];
-	var cardRegex=/^([0-9]{4}[\s-]?){3}([0-9]{4})$/;
+	var tester=true;
+	var cardRegex = /^([0-9]{4}[\s-]?){3}([0-9]{4})$/;
+	var pinRegex = /^[0-9]{3}$/;
+	var today = new Date();
+	var dateProvided = new Date();
+	dateProvided.setFullYear(expYear, expMonth, 1);
+
+	if(expMonth>=1 && expMonth<=12){
+		if(dateProvided<today){
+			errors.push("ERROR: The date provided is incorrect");
+			tester=false;
+		}
+	}
+	else{
+		errors.push("ERROR: The month provided doesn't make any sense");
+		tester=false
+	}
 	
+	if(cardRegex.test(ccNum)==false){
+		errors.push("ERROR: The credit card number provided is in the incorrect format\n" +
+				"-1111-2222-3333-4444 or 1111 2222 3333 4444 are " +
+				"the correct formats");
+		tester=false;
+	}
 	
+	if(pinRegex.test(cvv)==false){
+		errors.push("ERROR: The security code provided has an incorrect format");
+		tester=false;
+	}
+	
+	if(errors.length>0){
+		alert(errors.join("\n\n"))
+	}
+	
+	return tester;
 }
 
 function addItemToCart(address) {
